17883d5c23a2f9c679a3b0d0a18eae6dd36e5d99
ipikin <illia.pikin@dgngames.com>
2021-11-24 18:07:59 +0200

    added enhancements calculation

--- domain/attack.go
+++ domain/attack.go
@@ -23,30 +23,34 @@ func (u *Unit) ApplyTemporalImpact(impact DamageImpact) bool {
 	return false
 }
 
+func (u *Unit) CalculateAttackChance(target *Unit, impact DamageImpact) float32 {
+	// todo: find better formula
+	return u.TotalLuck(true) + impact.Chance
+}
+
 func (u *Unit) Attack(target *Unit, impact []DamageImpact) (Damage, []DamageImpact, bool) {
-	var damage Damage = Damage{}
-	var tempImpact []DamageImpact = []DamageImpact{}
+	var permanentDamage Damage = Damage{}
+	var temporalImpact []DamageImpact = []DamageImpact{}
 	var success bool = false
 	for _, imp := range impact {
-		// todo: find better formula?
-		chance := imp.Chance + u.Stats.Attributes.Luck - u.State.Curse
+		chance := u.CalculateAttackChance(target, imp)
 		if !util.CheckRandomChance(chance) {
 			continue
 		}
 		if imp.Duration != 0 {
 			imp.Chance = 0
 			if target.ApplyTemporalImpact(imp) {
-				tempImpact = append(tempImpact, imp)
+				temporalImpact = append(temporalImpact, imp)
 				success = true
 			}
 		} else {
-			damage.Accumulate(imp.Damage)
+			permanentDamage.Accumulate(imp.Damage)
 			success = true
 		}
 	}
 	if success {
-		damage.Enchance(u.Stats.Attributes)
-		damage = target.ApplyPermanentDamage(damage)
+		permanentDamage.Enchance(u.Stats.Attributes)
+		permanentDamage = target.ApplyPermanentDamage(permanentDamage)
 	}
-	return damage, tempImpact, success
+	return permanentDamage, temporalImpact, success
 }
--- domain/enhancement.go
+++ domain/enhancement.go
@@ -5,3 +5,56 @@ type UnitEnhancement struct {
 	UnitAttributes
 	UnitResistance
 }
+
+func (u *UnitEnhancement) Accumulate(enhancement UnitEnhancement) {
+	u.UnitBaseAttributes.Health += enhancement.UnitBaseAttributes.Health
+	u.UnitBaseAttributes.Mana += enhancement.UnitBaseAttributes.Mana
+	u.UnitBaseAttributes.Stamina += enhancement.UnitBaseAttributes.Stamina
+
+	u.UnitAttributes.Strength += enhancement.UnitAttributes.Strength
+	u.UnitAttributes.Physique += enhancement.UnitAttributes.Physique
+	u.UnitAttributes.Dexterity += enhancement.UnitAttributes.Dexterity
+	u.UnitAttributes.Endurance += enhancement.UnitAttributes.Endurance
+	u.UnitAttributes.Intelligence += enhancement.UnitAttributes.Intelligence
+	u.UnitAttributes.Luck += enhancement.UnitAttributes.Luck
+}
+
+func (u *Unit) TotalLuck(checkEquipment bool) float32 {
+	var luck float32 = u.Stats.Attributes.Luck - u.State.Curse
+	for _, e := range u.Enhancement {
+		luck += e.Luck
+	}
+	if !checkEquipment {
+		return luck
+	}
+	for _, item := range u.Items {
+		equipment, ok := AsEquipment(item)
+		if !ok || !equipment.Equipped {
+			continue
+		}
+		for _, e := range equipment.Enhancement {
+			luck += e.Luck
+		}
+	}
+	return luck
+}
+
+func (u *Unit) TotalEnhancement(checkEquipment bool) *UnitEnhancement {
+	var enhancement *UnitEnhancement = &UnitEnhancement{}
+	for _, e := range u.Enhancement {
+		enhancement.Accumulate(e.UnitEnhancement)
+	}
+	if !checkEquipment {
+		return enhancement
+	}
+	for _, item := range u.Items {
+		equipment, ok := AsEquipment(item)
+		if !ok || !equipment.Equipped {
+			continue
+		}
+		for _, e := range equipment.Enhancement {
+			enhancement.Accumulate(e)
+		}
+	}
+	return enhancement
+}
