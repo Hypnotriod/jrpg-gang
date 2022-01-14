e12507b3ed0614596467e547cb5457f888610c67
ipikin <illia.pikin@dgngames.com>
2022-01-14 18:19:29 +0200

    Inventory items formatter

---
+++ util/formatter.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func AsCommaSeparatedSlice(slice interface{}) string {
+	if reflect.TypeOf(slice).Kind() != reflect.Slice {
+		return ""
+	}
+	values := reflect.ValueOf(slice)
+	var sb strings.Builder
+	for i := 0; i < values.Len(); i++ {
+		sb.WriteString(fmt.Sprintf("{%v}", values.Index(i)))
+		if i < values.Len()-1 {
+			sb.WriteString(", ")
+		}
+	}
+	return sb.String()
+}
