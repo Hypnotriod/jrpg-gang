01a7a310591babc65c582f450a6cc4ad072831a2
unknown <illia.pikin@gmail.com>
2022-02-19 20:01:08 +0200

    refactoring

--- controller/unit_factory.go
+++ controller/unit_factory.go
@@ -66,7 +66,9 @@ func newGameUnitTank() *engine.GameUnit {
 				"hand": 2,
 				"leg": 2,
 				"weapon": 2
-			}
+			},
+			"damage": [],
+			"modification": []
 		}`)
 	return unit.(*engine.GameUnit)
 }
@@ -119,7 +121,9 @@ func newGameUnitRogue() *engine.GameUnit {
 				"hand": 2,
 				"leg": 2,
 				"weapon": 2
-			}
+			},
+			"damage": [],
+			"modification": []
 		}`)
 	return unit.(*engine.GameUnit)
 }
@@ -172,7 +176,9 @@ func newGameUnitMage() *engine.GameUnit {
 				"hand": 2,
 				"leg": 2,
 				"weapon": 2
-			}
+			},
+			"damage": [],
+			"modification": []
 		}`)
 	return unit.(*engine.GameUnit)
 }
