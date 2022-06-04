fd6b2021add799c68e3d3697d6d06dc38cccb8ae
Ilya Pikin <hypnotiod@gmail.com>
2022-06-04 20:54:12 +0300

    nickname regex update

--- controller/constants.go
+++ controller/constants.go
@@ -1,4 +1,4 @@
 package controller
 
-const USER_NICKNAME_REGEX string = `^[a-zA-Z][a-zA-Z0-9-_]+$`
+const USER_NICKNAME_REGEX string = `^[a-zA-Z0-9-_ ]{4,20}$`
 const GAME_ROOM_MAX_CAPACITY uint = 4
