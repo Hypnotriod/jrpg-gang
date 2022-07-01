6db69c1106fa0f73116769d30fddc58efa6cd57e
ipikin <illia.pikin@dgngames.com>
2022-07-01 10:52:54 +0300

    added race conditions protection

--- util/timer.go
+++ util/timer.go
@@ -1,25 +1,23 @@
 package util
 
 import (
+	"sync"
 	"time"
 )
 
 type Timer struct {
-	timer  *time.Timer
-	cancel chan struct{}
+	mutex     sync.Mutex
+	timer     *time.Timer
+	cancel    chan struct{}
+	cancelled bool
+	completed bool
 }
 
 func NewTimer(duration time.Duration, complete func()) *Timer {
 	t := &Timer{}
 	t.timer = time.NewTimer(duration)
 	t.cancel = make(chan struct{})
-	go func() {
-		select {
-		case <-t.timer.C:
-			complete()
-		case <-t.cancel:
-		}
-	}()
+	go t.wait(complete, func() {})
 	return t
 }
 
@@ -27,18 +25,32 @@ func NewTimerWithCancel(duration time.Duration, complete func(), cancel func()) 
 	t := &Timer{}
 	t.timer = time.NewTimer(duration)
 	t.cancel = make(chan struct{})
-	go func() {
-		select {
-		case <-t.timer.C:
-			complete()
-		case <-t.cancel:
-			cancel()
-		}
-	}()
+	go t.wait(complete, cancel)
 	return t
 }
 
 func (t *Timer) Cancel() {
-	t.timer.Stop()
-	t.cancel <- struct{}{}
+	t.mutex.Lock()
+	if !t.completed {
+		t.cancelled = true
+		t.timer.Stop()
+		t.cancel <- struct{}{}
+	}
+	t.mutex.Unlock()
+}
+
+func (t *Timer) wait(complete func(), cancel func()) {
+	for !t.completed && !t.cancelled {
+		select {
+		case <-t.timer.C:
+			t.mutex.Lock()
+			if !t.cancelled {
+				t.completed = true
+				complete()
+			}
+			t.mutex.Unlock()
+		case <-t.cancel:
+			cancel()
+		}
+	}
 }
