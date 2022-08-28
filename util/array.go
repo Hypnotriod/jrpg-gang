0bd0d0cda705b9188e467019dc2997550707f224
ipikin <illia.pikin@gmail.com>
2022-08-27 19:28:46 -0700

    refactoring

--- util/array.go
+++ util/array.go
@@ -18,98 +18,98 @@ func IndexOf[T Ordered](values []T, value T) int {
 	return -1
 }
 
-func Find[T any](values []T, test func(value *T) bool) *T {
+func Find[T any](values []T, predicate func(value *T) bool) *T {
 	for i := range values {
-		if test(&values[i]) {
+		if predicate(&values[i]) {
 			return &values[i]
 		}
 	}
 	return nil
 }
 
-func Findp[T any](values []*T, test func(value *T) bool) *T {
+func Findp[T any](values []*T, predicate func(value *T) bool) *T {
 	for i := range values {
-		if test(values[i]) {
+		if predicate(values[i]) {
 			return values[i]
 		}
 	}
 	return nil
 }
 
-func Any[T any](values []T, test func(value *T) bool) bool {
+func Any[T any](values []T, predicate func(value *T) bool) bool {
 	for i := range values {
-		if test(&values[i]) {
+		if predicate(&values[i]) {
 			return true
 		}
 	}
 	return false
 }
 
-func Anyp[T any](values []*T, test func(value *T) bool) bool {
+func Anyp[T any](values []*T, predicate func(value *T) bool) bool {
 	for i := range values {
-		if test(values[i]) {
+		if predicate(values[i]) {
 			return true
 		}
 	}
 	return false
 }
 
-func Every[T any](values []T, test func(value *T) bool) bool {
+func Every[T any](values []T, predicate func(value *T) bool) bool {
 	if len(values) == 0 {
 		return false
 	}
 	for i := range values {
-		if !test(&values[i]) {
+		if !predicate(&values[i]) {
 			return false
 		}
 	}
 	return true
 }
 
-func Everyp[T any](values []*T, test func(value *T) bool) bool {
+func Everyp[T any](values []*T, predicate func(value *T) bool) bool {
 	if len(values) == 0 {
 		return false
 	}
 	for i := range values {
-		if !test(values[i]) {
+		if !predicate(values[i]) {
 			return false
 		}
 	}
 	return true
 }
 
-func Filter[T any](values []T, test func(value *T) bool) []T {
+func Filter[T any](values []T, predicate func(value *T) bool) []T {
 	result := []T{}
 	for i := range values {
-		if test(&values[i]) {
+		if predicate(&values[i]) {
 			result = append(result, values[i])
 		}
 	}
 	return result
 }
 
-func Filterp[T any](values []*T, test func(value *T) bool) []*T {
+func Filterp[T any](values []*T, predicate func(value *T) bool) []*T {
 	result := []*T{}
 	for i := range values {
-		if test(values[i]) {
+		if predicate(values[i]) {
 			result = append(result, values[i])
 		}
 	}
 	return result
 }
 
-func Map[T any, J any](values []T, convert func(value *T) J) []J {
+func Map[T any, J any](values []T, mapper func(value *T) J) []J {
 	result := make([]J, 0, len(values))
 	for i := range values {
-		result = append(result, convert(&values[i]))
+		result = append(result, mapper(&values[i]))
 	}
 	return result
 }
 
-func Mapp[T any, J any](values []*T, convert func(value *T) *J) []*J {
+func Mapp[T any, J any](values []*T, mapper func(value *T) *J) []*J {
 	result := []*J{}
 	for i := range values {
-		result = append(result, convert(values[i]))
+		result = append(result, mapper(values[i]))
 	}
 	return result
 }
