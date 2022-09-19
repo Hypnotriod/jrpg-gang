560cc4c2cdf0e8f0325d860ece9c934dcb308d0e
ipikin <illia.pikin@dgngames.com>
2022-09-19 18:55:22 +0300

    updated types

--- util/types.go
+++ util/types.go
@@ -1,11 +1,19 @@
 package util
 
+type UnsignedInteger interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type SignedInteger interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
 type Integer interface {
-	int | int16 | int32 | int64 | uint | uint16 | uint32 | uint64
+	SignedInteger | UnsignedInteger
 }
 
 type Float interface {
-	float32 | float64
+	~float32 | ~float64
 }
 
 type Number interface {
