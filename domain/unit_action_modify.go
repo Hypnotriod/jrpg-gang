0cd82f4a4bdcecb2748a47be5318bde469585a1d
ipikin <illia.pikin@dgngames.com>
2022-12-02 20:11:40 +0200

    removed chance field from temporary modification object

--- domain/unit_action_modify.go
+++ domain/unit_action_modify.go
@@ -7,6 +7,7 @@ func (u *Unit) Modify(target *Unit, modification []UnitModificationImpact) ([]Un
 		if ench.Chance != 0 && !u.CheckRandomChance(u.CalculateModificationChance(ench)) {
 			break
 		}
+		ench.Chance = 0
 		if ench.Duration != 0 {
 			target.Modification = append(target.Modification, ench)
 			temporalModification = append(temporalModification, ench)
