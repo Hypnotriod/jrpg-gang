0219589fc6f4cf64d3a4354ed19a1c3fdfe9b4b3
illia <illia.pikin@gmail.com>
2023-03-19 10:27:17 +0200

    fix

--- util/map.go
+++ util/map.go
@@ -1,7 +1,7 @@
 package util
 
 func CloneMap[K Ordered, V any](src map[K]V) map[K]V {
-	dts := make(map[K]V)
+	dts := make(map[K]V, len(src))
 	for key, value := range src {
 		dts[key] = value
 	}
