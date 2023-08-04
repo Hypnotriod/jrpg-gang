6fe3fcc30f4000fc878abf8db135db007065df1a
illia <illia.pikin@gmail.com>
2023-08-04 20:02:29 +0300

    fix action points calculation

--- domain/unit.go
+++ domain/unit.go
@@ -110,7 +110,7 @@ func (u *Unit) TotalLuck() float32 {
 
 func (u *Unit) TotalActionPoints() float32 {
 	var actionPoints float32 = u.Stats.BaseAttributes.ActionPoints
-	actionPoints += float32(u.Stats.Attributes.Initiative / 10)
+	actionPoints += float32(uint(u.Stats.Attributes.Initiative) / 10)
 	for _, ench := range u.Modification {
 		actionPoints += ench.BaseAttributes.ActionPoints
 	}
