7c1b40d7e0661c2f3d03143e19511e546f4ee137
illia <illia.pikin@gmail.com>
2025-02-25 11:01:28 +0200

    refactoring

--- session/routes.go
+++ session/routes.go
@@ -10,16 +10,21 @@ import (
 func (h *Hub) initRoutes(router *mux.Router) {
 	router.HandleFunc("/google/oauth2", h.auth.HandleGoogleAuth2).Methods(http.MethodGet)
 	router.HandleFunc("/google/oauth2/callback", h.auth.HandleGoogleAuth2Callback).Methods(http.MethodGet)
-	router.HandleFunc("/configuration", h.handleConfigurationRequest).
-		Methods(http.MethodPost, http.MethodOptions).Headers(HeaderContentType, ContentTypeApplicationJson)
+	router.HandleFunc("/configuration", h.handleConfigurationRequest).Methods(http.MethodPost).
+		Headers(HeaderContentType, ContentTypeApplicationJson)
+	router.NotFoundHandler = http.HandlerFunc(h.handleNotFoundRequest)
 }
 
 func (h *Hub) handleConfigurationRequest(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add(HeaderContentType, ContentTypeApplicationJson)
 	r.Body = http.MaxBytesReader(w, r.Body, ConfigurationPayloadMaxSize)
 	if data, err := io.ReadAll(r.Body); err == nil {
+		w.Header().Add(HeaderContentType, ContentTypeApplicationJson)
 		w.Write(h.controller.HandleConfigurationRequest(data))
 	} else {
-		http.Error(w, "", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 	}
+}
+
+func (h *Hub) handleNotFoundRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
 }
